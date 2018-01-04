e7a746fab821fa190781986d606f76b8530e3284
Håkon Krogh <hakonhk@gmail.com>
2018-01-04 11:52:50 +0100

    Added custom server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+
+    // Handle your own routes here
+    if (pathname === '/pagetwo') {
+      app.render(req, res, '/page2', query);
+    } else if (pathname === '/frontpage') {
+      app.render(req, res, '/', query);
+    } else {
+      // Just let Next handle the request
+      handle(req, res, parsedUrl);
+    }
+  }).listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
