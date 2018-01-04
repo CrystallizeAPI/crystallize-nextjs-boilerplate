92adbeb0a3f1065fd1378c5cc4b271cb6bf26e18
Håkon Krogh <hakonhk@gmail.com>
2018-01-04 13:26:21 +0100

    Improved route config

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const { createServer } = require('http');
-const { parse } = require('url');
 const next = require('next');
+const { parse } = require('url');
+
+const { PageMatchForRequest } = require('./routes');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -10,15 +12,10 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-
-    // Handle your own routes here
-    if (pathname === '/pagetwo') {
-      app.render(req, res, '/page2', query);
-    } else if (pathname === '/frontpage') {
-      app.render(req, res, '/', query);
+    const pageMatch = PageMatchForRequest(req);
+    if (pageMatch) {
+      app.render(req, res, pageMatch, query);
     } else {
-      // Just let Next handle the request
       handle(req, res, parsedUrl);
     }
   }).listen(port, err => {
---
+++ server/routes.js
@@ -0,0 +1,21 @@
+/**
+ * Check if a given request matches one of your entry pages
+ * @param {Object} url - The URL object
+ * @param {Object} url.pathname - The relative path name
+ * @param {Object} url.query - The request query
+ * @returns {(string|boolean)} The component name or false if not found
+ */
+function PageMatchForRequest({ pathname, query }) {
+  if (pathname === '/pagetwo') {
+    return '/page2';
+  } else if (pathname === '/frontpage') {
+    return '/';
+  }
+
+  // No matching page found for the the request
+  return false;
+}
+
+module.exports = {
+  PageMatchForRequest
+};
