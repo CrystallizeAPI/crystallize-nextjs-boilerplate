ef663a24e4380f70bf9110faf3cebed697abe103
Brendan Goodenough <brendan@goodenough.nz>
2019-08-13 15:08:47 +1200

    Remove server folder

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 /* eslint no-underscore-dangle: "off" */
 const {
   NODE_ENV = 'development',
-  PORT = 3000,
   CRYSTALLIZE_TENANT_ID = 'demo',
   CRYSTALLIZE_GRAPH_URL_BASE = 'https://graph.crystallize.com',
   GTM_ID
@@ -11,8 +10,6 @@ const DEV = NODE_ENV !== 'production';
 
 global.__crystallizeConfig = {
   NODE_ENV,
-  PORT: parseInt(PORT, 10),
-  HOST_NAME: `http://localhost:${PORT}`,
   TENANT_ID: CRYSTALLIZE_TENANT_ID,
   GRAPH_URL: `${CRYSTALLIZE_GRAPH_URL_BASE}/tenant/${CRYSTALLIZE_TENANT_ID}`,
   GTM_ID,
--- server/lib/klarna.js
+++
@@ -1,6 +0,0 @@
-const KlarnaV2 = require('@crystallize/node-klarna/v2');
-
-module.exports = new KlarnaV2({
-  testDrive: true,
-  useTestMerchant: true
-});
