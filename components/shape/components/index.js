1e1531e1cceee45c4905f1e36f07279192871682
Håkon Krogh <hakonhk@gmail.com>
2019-08-21 09:29:59 +0200

    Only render shape components with content

--- components/shape/components/index.js
+++ components/shape/components/index.js
@@ -9,31 +9,33 @@ const ShapeComponents = ({ components }) => {
     return null;
   }
 
-  return components.map(({ type, ...component }, index) => {
-    const key = index;
+  return components
+    .filter(component => component.content != null)
+    .map(({ type, ...component }, index) => {
+      const key = index;
 
-    if (type === 'paragraphCollection') {
-      return (
-        <ParagraphCollection
-          key={key}
-          paragraphs={component.content.paragraphs}
-        />
-      );
-    }
+      if (type === 'paragraphCollection') {
+        return (
+          <ParagraphCollection
+            key={key}
+            paragraphs={component.content.paragraphs}
+          />
+        );
+      }
 
-    if (type === 'richText') {
-      return <Chunk key={key} {...component.content.json[0]} />;
-    }
+      if (type === 'richText') {
+        return <Chunk key={key} {...component.content.json[0]} />;
+      }
 
-    if (type === 'singleLine') {
-      return <span key={key}>{component.content.text}</span>;
-    }
+      if (type === 'singleLine') {
+        return <span key={key}>{component.content.text}</span>;
+      }
 
-    // eslint-disable-next-line no-console
-    console.log(`Render for ${type} not implemented`);
+      // eslint-disable-next-line no-console
+      console.log(`Render for ${type} not implemented`);
 
-    return <span key={key} />;
-  });
+      return <span key={key} />;
+    });
 };
 
 ShapeComponents.propTypes = {
