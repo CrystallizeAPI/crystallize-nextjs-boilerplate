fda88d1319a8cfc7629fd5098e850a2c21b4e052
Brendan Goodenough <brendan@goodenough.nz>
2019-08-28 17:42:20 +1200

    Remove container from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ApolloProvider, Query } from 'react-apollo';
 import { IntlProvider } from 'react-intl';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 
 import AuthGate from 'components/auth-context';
 import withData from 'lib/with-data';
@@ -13,28 +13,26 @@ class MyApp extends App {
     const { Component, pageProps, apolloClient } = this.props;
 
     return (
-      <Container>
-        <ApolloProvider client={apolloClient}>
-          <Query
-            variables={{ path: '/', language: 'en' }}
-            query={FETCH_TREE_NODE_AND_MENU}
-          >
-            {({ loading, error, data }) => {
-              if (loading) return null;
-              if (error) return null;
-              return (
-                <IntlProvider locale={data.tenant.defaults.language}>
-                  <BasketProvider>
-                    <AuthGate>
-                      <Component {...pageProps} />;
-                    </AuthGate>
-                  </BasketProvider>
-                </IntlProvider>
-              );
-            }}
-          </Query>
-        </ApolloProvider>
-      </Container>
+      <ApolloProvider client={apolloClient}>
+        <Query
+          variables={{ path: '/', language: 'en' }}
+          query={FETCH_TREE_NODE_AND_MENU}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return null;
+            if (error) return null;
+            return (
+              <IntlProvider locale={data.tenant.defaults.language}>
+                <BasketProvider>
+                  <AuthGate>
+                    <Component {...pageProps} />;
+                  </AuthGate>
+                </BasketProvider>
+              </IntlProvider>
+            );
+          }}
+        </Query>
+      </ApolloProvider>
     );
   }
 }
