29737c453ecadba19e55c41234e4ee94a900002e
Håkon Krogh <hakonhk@gmail.com>
2019-08-29 10:22:58 +0200

    Ensure both main queries are executed on server for Now deployments

--- pages/catalog/index.js
+++ pages/catalog/index.js
@@ -5,10 +5,18 @@ import Layout from 'components/layout';
 import DocumentPage from 'page-components/document';
 import FolderPage from 'page-components/folder';
 import ProductPage from 'page-components/product';
-import { useTreeNodeQuery } from 'lib/graph';
+import { useTreeNodeQuery, useMenuAndTenantQuery } from 'lib/graph';
 
 export default function CatalogPage() {
+  /**
+   * Get both the current component at the path and
+   * also the top menu and tenant settings. We do both
+   * here since the menu and tenant query most likely
+   * will be executed later anyways. This approach will
+   * save a good handful of milliseconds
+   */
   const { fetching, error, data } = useTreeNodeQuery();
+  const menuAndTenantResult = useMenuAndTenantQuery();
 
   if (fetching) {
     return <Layout loading />;
@@ -21,6 +29,11 @@ export default function CatalogPage() {
   // Nothing in Crystallize at this path. Show 404 page
   if (!data.tree || !data.tree.length) {
     return <Error statusCode="404" />;
+  }
+
+  if (menuAndTenantResult.data && typeof window === 'undefined') {
+    // eslint-disable-next-line no-console
+    console.log('Populated cache for tenant and menu');
   }
 
   const { tree } = data;
