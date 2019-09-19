b32ca186db565f04e73d85a641246f7d63cd4323
Brendan Goodenough <brendan@goodenough.nz>
2019-09-20 09:45:13 +1200

    Add header to folder

--- page-components/folder/index.js
+++ page-components/folder/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Grid } from '@crystallize/grid-renderer/react';
-import { Outer, Header } from 'ui';
+import { Outer, Header, H1 } from 'ui';
 import Layout from 'components/layout';
 import Product from 'components/category-item';
 import ShapeComponents from 'components/shape/components';
@@ -24,6 +24,7 @@ export default class FolderPage extends React.PureComponent {
       <Layout title={folder.name}>
         <Outer>
           <Header>
+            <H1>{folder.name}</H1>
             <ShapeComponents components={folder.components} />
           </Header>
           {children ? (
