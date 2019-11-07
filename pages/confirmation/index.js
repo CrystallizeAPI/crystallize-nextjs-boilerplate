2f7da4d8234b52d744e9af189e355383b901ecb9
Brendan Goodenough <brendan@goodenough.nz>
2019-11-07 10:23:09 +0100

    Refactor order summary to clear basket

--- pages/confirmation/index.js
+++ pages/confirmation/index.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import queryString from 'query-string';
 import styled from 'styled-components';
 
 import Layout from 'components/layout';
+import { BasketContext } from 'components/basket';
 import BillingDetails from 'components/billing-details';
 import OrderItems from 'components/order-items';
 import { H1, Outer, Header, responsive } from 'ui';
@@ -15,58 +16,76 @@ export const Inner = styled.div`
   }
 `;
 
-const Confirmation = ({ orderId, paymentMethod }) => {
-  const [orderData, setOrder] = useState({});
+class Confirmation extends React.Component {
+  state = {
+    emptied: false,
+    orderData: null
+  };
 
-  useEffect(() => {
-    async function fetchData() {
-      let url = `/api/order-confirmation?order_id=${orderId}`;
-      if (paymentMethod) url = `${url}&payment_method=${paymentMethod}`;
-      const response = await fetch(url);
-      const order = await response.json();
-
-      return setOrder(order);
-    }
-    fetchData();
-  }, [orderId, paymentMethod]);
-
-  if (!orderData.data) {
-    return <Layout loading />;
+  static async getInitialProps({ req }) {
+    const { query } = queryString.parseUrl(req.url);
+    return { orderId: query.order_id, paymentMethod: query.payment_method };
   }
 
-  const order = orderData.data.orders.get;
-  const { email } = order.customer.addresses[0];
+  componentDidMount() {
+    const { orderId, paymentMethod } = this.props;
+    this.empty();
 
-  const items = order.cart.map(item => ({
-    ...item,
-    image: {
-      url: item.imageUrl
-    },
-    price: item.price.net
-  }));
+    let url = `/api/order-confirmation?order_id=${orderId}`;
+    if (paymentMethod) url = `${url}&payment_method=${paymentMethod}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(orderData => this.setState({ orderData }));
+  }
 
-  return (
-    <Layout title="Order Summary">
-      <Outer>
-        <Header>
-          <H1>Order Summary</H1>
-          <p>
-            Your order (<strong>#{orderId}</strong>) has been confirmed. A copy
-            of your order has been sent to <strong>{email}</strong>.
-          </p>
-        </Header>
-        <Inner>
-          <OrderItems items={items} />
-          <BillingDetails order={order} />
-        </Inner>
-      </Outer>
-    </Layout>
-  );
-};
+  static contextType = BasketContext;
 
-Confirmation.getInitialProps = async ({ req }) => {
-  const { query } = queryString.parseUrl(req.url);
-  return { orderId: query.order_id, paymentMethod: query.payment_method };
-};
+  empty() {
+    const { emptied } = this.state;
+    const { actions } = this.context;
+    if (!emptied) {
+      actions.empty();
+      this.setState({ emptied: true });
+    }
+  }
+
+  render() {
+    const { orderId } = this.props;
+    const { orderData } = this.state;
+
+    if (!orderData || !orderData.data) {
+      return <Layout loading />;
+    }
+
+    const order = orderData.data.orders.get;
+    const { email } = order.customer.addresses[0];
+
+    const items = order.cart.map(item => ({
+      ...item,
+      image: {
+        url: item.imageUrl
+      },
+      price: item.price.net
+    }));
+
+    return (
+      <Layout title="Order Summary">
+        <Outer>
+          <Header>
+            <H1>Order Summary</H1>
+            <p>
+              Your order (<strong>#{orderId}</strong>) has been confirmed. A
+              copy of your order has been sent to <strong>{email}</strong>.
+            </p>
+          </Header>
+          <Inner>
+            <OrderItems items={items} />
+            <BillingDetails order={order} />
+          </Inner>
+        </Outer>
+      </Layout>
+    );
+  }
+}
 
 export default Confirmation;
