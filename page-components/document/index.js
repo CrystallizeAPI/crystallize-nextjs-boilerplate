f0356e6d2ee48391e117533950efb72429330d30
Håkon Krogh <hakonhk@gmail.com>
2020-01-17 16:00:40 +0100

    Fixed document rendering

--- page-components/document/index.js
+++ page-components/document/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
+import { useSettings } from 'components/settings-context';
+import { useSafePathQuery } from 'lib/graph';
 import { Outer, H1 } from 'ui';
 import Layout from 'components/layout';
 import ShapeComponents from 'components/shape/components';
 
+import folderPageQuery from './query';
 import { Document } from './styles';
 
-const DocumentPage = ({ data }) => {
+const DocumentPage = () => {
+  const { language } = useSettings();
+  const router = useRouter();
+
+  const [queryResult] = useSafePathQuery({
+    query: folderPageQuery,
+    variables: {
+      language,
+      path: router.asPath
+    }
+  });
+
+  const { fetching, error, data } = queryResult;
+
+  if (fetching) {
+    return <Layout loading />;
+  }
+
+  if (error) {
+    return <Layout error />;
+  }
+
   const [document] = data.tree;
+  if (!document) {
+    return <Layout error />;
+  }
 
   return (
     <Layout title={document.name}>
