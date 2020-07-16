c6bec70c18a5b8697d39374187e6ba94b299471d
Håkon Krogh <hakonhk@gmail.com>
2020-07-16 09:47:09 +0200

    Update card style

--- src/page-components/checkout/payment/stripe/form/styles.js
+++ src/page-components/checkout/payment/stripe/form/styles.js
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
+import is from 'styled-is';
 
 export const CardElementWrapper = styled.div`
   border-bottom: 1px solid var(--color-box-background);
   padding: 0.3rem 0;
   margin-bottom: 1rem;
   transition: border 0.2s ease-in-out;
+
+  ${is('card-error')`
+    border-color: var(--color-error);
+  `};
+
+  ${is('card-ok')`
+    border-color: var(--color-text-main);
+  `};
 `;
 
 export const ErrorMessage = styled.p`
