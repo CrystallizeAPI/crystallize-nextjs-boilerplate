617d86793afd314c19cfeed6bc2e8ae5b060e653
Håkon Krogh <hakonhk@gmail.com>
2020-10-13 08:42:12 +0200

    Use latest node-klarna version

--- src/lib-api/payment-providers/klarna/index.js
+++ src/lib-api/payment-providers/klarna/index.js
@@ -3,7 +3,13 @@ import { Klarna } from '@crystallize/node-klarna';
 export { default as orderNormalizer } from './order-normalizer';
 export { default as orderDenormalizer } from './order-denormalizer';
 
+/**
+ * Read more about how to talk to the Klarna API here:
+ * https://developers.klarna.com/api/#introduction
+ */
+
 export const client = new Klarna({
   username: process.env.KLARNA_USERNAME,
-  password: process.env.KLARNA_PASSWORD
+  password: process.env.KLARNA_PASSWORD,
+  apiEndpoint: 'https://api.playground.klarna.com'
 });
