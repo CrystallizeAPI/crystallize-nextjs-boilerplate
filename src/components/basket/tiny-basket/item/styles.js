ca66abf3ca334a9635ee88a5f0c8c100a0125fea
Håkon Krogh <hakonhk@gmail.com>
2020-10-16 08:37:00 +0200

    Fixed styles for minibasket

--- src/components/basket/tiny-basket/item/styles.js
+++ src/components/basket/tiny-basket/item/styles.js
@@ -72,10 +72,13 @@ export const ItemImage = styled(CrystallizeImage).attrs(() => ({
   sizes: '50px'
 }))`
   width: 50px;
+  position: relative;
+
   img {
-    top: 50%;
-    position: relative;
-    transform: translateY(-50%);
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
   }
 `;
 
