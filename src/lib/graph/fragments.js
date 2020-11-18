dd16352304b7b6bb7f77de05e8a017d4a5f10a38
didrikhegna <didrik@snowball.digital>
2020-11-18 15:43:06 +0100

    fixed schemas for products

--- src/lib/graph/fragments.js
+++ src/lib/graph/fragments.js
@@ -69,6 +69,7 @@ export default `
 
   fragment richText on RichTextContent {
     json
+    plainText
   }
 
   fragment image on Image {
--- src/page-components/product/index.js
+++ src/page-components/product/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Image as Img } from '@crystallize/react-image';
 import ContentTransformer from 'ui/content-transformer';
-
 import { simplyFetchFromGraph } from 'lib/graph';
 import { screen } from 'ui';
 import Layout from 'components/layout';
@@ -11,6 +10,8 @@ import VariantSelector from './variant-selector';
 import Buy from './buy';
 import query from './query';
 import Topics from 'components/topics';
+import { useLocale } from 'lib/app-config';
+import { useRouter } from 'next/router';
 
 import {
   Outer,
@@ -38,6 +39,8 @@ export async function getData({ asPath, language, preview = null }) {
 }
 
 export default function ProductPage({ product, preview }) {
+  const locale = useLocale();
+  const router = useRouter();
   // Set the selected variant to the default
   const [selectedVariant, setSelectedVariant] = useState(
     product.variants.find((v) => v.isDefault)
@@ -58,8 +61,44 @@ export default function ProductPage({ product, preview }) {
     (c) => !['Summary', 'Description', 'Specs'].includes(c.name)
   );
 
+  let schema = [];
+  product.variants.map((variant) => {
+    const { price, currency } =
+      variant.priceVariants.find(
+        (pv) => pv.identifier === locale.crystallizePriceVariant
+      ) || {};
+    schema = [
+      ...schema,
+      {
+        '@context': 'https://schema.org/',
+        '@type': 'Product',
+        sku: variant?.sku,
+        name: variant?.name,
+        description: summaryComponent?.content?.plainText?.[0],
+        image: variant?.images?.[0]?.url,
+        offers: {
+          '@type': 'Offer',
+          // priceValidUntil: 'YYYY-MM-DD',
+          priceCurrency: currency,
+          url: router?.asPath,
+          availability:
+            variant?.stock > 0
+              ? `https://schema.org/InStock`
+              : `https://schema.org/OutOfStock`,
+          price,
+          currency
+        }
+      }
+    ];
+  });
   return (
     <Layout title={product.name} preview={preview}>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(schema)
+        }}
+      />
       <Outer>
         <Sections>
           <Media>
