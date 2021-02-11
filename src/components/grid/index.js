fb837049e79c93bf9cd382bd9263754215732418
Victor Ribero <victor.ribero3@gmail.com>
2021-02-10 17:21:47 -0700

    Fix: display videos correctly when used in a grid for small devices

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -10,8 +10,22 @@ const StyledGrid = styled(GridRenderer)`
     `repeat(${p.model.rows?.length}, var(--grid-row-height))`};
 
   ${responsive.xs} {
-    display: block !important;
-    grid-template-columns: 100% !important;
+    // We force the grid to have 1 column for small screens (display items vertically)
+    // and overwrite the layout customized from the PIM dashboard.
+    // The usage of the "!important" keyword is needed to overwrite the inline
+    // styles added in the Crystallize "grid-renderer" component at render-level.
+    // Link to the component: https://github.com/CrystallizeAPI/grid-renderer
+    // Link to the implementation: https://github.com/CrystallizeAPI/grid-renderer/blob/master/src/react/css-grid.js#L18
+    grid-template-columns: 1fr !important;
+    grid-template-rows: unset !important;
+    grid-auto-rows: var(--grid-row-height);
+
+    // We also force some styles to each cell (inmediate child) because they add inline styles too
+    // Link to the implementation: https://github.com/CrystallizeAPI/grid-renderer/blob/master/src/react/css-grid.js#L31
+    > * {
+      grid-column: initial !important;
+      grid-row: initial !important;
+    }
   }
 `;
 
