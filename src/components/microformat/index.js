9f16d65e426940cdcd40850133389522c9788e04
Victor Ribero <victor.ribero3@gmail.com>
2021-02-17 07:36:33 -0700

    remove unused old microformat (duplicated code)

--- src/components/microformat/index.js
+++ src/components/microformat/index.js
@@ -1,17 +1,17 @@
-import DocumentItem from './document-item';
+import DocumentShapeMicroformat from './document-item';
 import ProductShapeMicroformat from 'shapes/product/microformat';
 import FolderItem from './folder-item';
 
-export default function Microformat({ item, locale }) {
+export default function Microformat({ item }) {
   if (!item) {
     return null;
   }
 
-  const commonProps = { key: item.path, locale };
+  const commonProps = { key: item.path };
   const types = {
     product: <ProductShapeMicroformat {...commonProps} data={item} />,
     folder: <FolderItem {...commonProps} data={item} />,
-    document: <DocumentItem {...commonProps} data={item} />
+    document: <DocumentShapeMicroformat {...commonProps} data={item} />
   };
 
   return types[item.type] || null;
