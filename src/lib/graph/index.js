fdff64f55d4f08c8a9a0dc474ddb168efadcd0a8
Håkon Krogh <hakonhk@gmail.com>
2021-05-12 18:53:20 +0200

    Easier switch to proxied API

--- src/lib/graph/index.js
+++ src/lib/graph/index.js
@@ -24,8 +24,20 @@ export function safePathQuery({ variables, ...rest }) {
   };
 }
 
+/**
+ * If you have enabled authentication on your APIs, then you need to
+ * proxy the requests through the Service API:
+ * const apiUrlBase = process.env.NEXT_PUBLIC_SERVICE_API_URL.replace(
+ *  '/graphql',
+ *  '/crystallize'
+ * );
+ * Read more about authentication for the APIs here:
+ * https://crystallize.com/learn/developer-guides/api-overview/authentication
+ */
+const apiUrlBase = `https://api.crystallize.com/${process.env.NEXT_PUBLIC_CRYSTALLIZE_TENANT_IDENTIFIER}`;
+
 export async function simplyFetchFromGraph({
-  uri = `https://api.crystallize.com/${process.env.NEXT_PUBLIC_CRYSTALLIZE_TENANT_IDENTIFIER}/catalogue`,
+  uri = `${apiUrlBase}/catalogue`,
   query,
   variables
 }) {
@@ -48,7 +60,7 @@ export async function simplyFetchFromGraph({
 
 export function simplyFetchFromSearchGraph(args) {
   return simplyFetchFromGraph({
-    uri: `https://api.crystallize.com/${process.env.NEXT_PUBLIC_CRYSTALLIZE_TENANT_IDENTIFIER}/search`,
+    uri: `${apiUrlBase}/search`,
     ...args
   });
 }
